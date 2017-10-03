a7c560dc8ecd9aca44238979bbb425449b115654
Bret Farley <bretfarley12@mittymonarchl.com>
2017-10-02 22:24:40 -0700

    init commit

---
+++ me.py
@@ -0,0 +1,11 @@
+me = {
+    "name" : "Bret",
+    "age" : "23",
+    "country": "The United States",
+    "language": "Python"
+}
+
+print "My name is {}".format(me["name"])
+print "My age is {}".format(me["age"])
+print "My country of birth is {}".format(me["country"])
+print "My favorite language is {}".format(me["language"])
